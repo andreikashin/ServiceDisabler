0b65b338e2fc69036cb156c89744935bd126ea00
andrei <andrei.kashin@gmail.com>
2018-03-19 19:29:18 +0200

    Add extension methods

---
+++ ServiceDisabler/Helpers/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDisabler.Helpers
+{
+    internal static class Extensions
+    {
+        public static DateTime SetTime(this DateTimeOffset currentDateTime, int hours, int minutes, int seconds)
+        {
+            return new DateTime(
+                currentDateTime.Year,
+                currentDateTime.Month,
+                currentDateTime.Day,
+                hours, minutes, seconds);
+        }
+    }
+}
